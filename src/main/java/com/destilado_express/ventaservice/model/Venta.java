8fbf854651cdf04f816719282ab52a5cc191f340
Francisco Valdes <ecodisonante@gmail.com>
2024-12-06 21:27:27 -0300

    feat: Actualizacion de modelos

--- src/main/java/com/destilado_express/ventaservice/model/Venta.java
+++ src/main/java/com/destilado_express/ventaservice/model/Venta.java
@@ -1,14 +1,17 @@
 package com.destilado_express.ventaservice.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.Data;
 
 @Entity
@@ -22,16 +25,25 @@ public class Venta {
     @Column(nullable = false)
     private Long userId;
 
-    @Column(nullable = false)
-    private Double montoTotal;
+    @OneToMany(mappedBy = "venta", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<VentaProducto> productos;
 
     @Column(nullable = false)
-    private LocalDateTime fecha = LocalDateTime.now();
-
-    @ManyToOne
-    @JoinColumn(name = "forma_pago_id", nullable = false)
-    private FormaPago formaPago;
+    private Boolean activa = true;
 
     @Column(nullable = false)
-    private Boolean finalizada;
+    private LocalDateTime created;
+
+    @Column(nullable = true)
+    private LocalDateTime updated;
+
+    @PrePersist
+    protected void onCreate() {
+        this.created = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updated = LocalDateTime.now();
+    }
 }
--- src/main/java/com/destilado_express/ventaservice/model/VentaProducto.java
+++ src/main/java/com/destilado_express/ventaservice/model/VentaProducto.java
@@ -1,20 +1,30 @@
 package com.destilado_express.ventaservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
 @Data
-@IdClass(VentaProductoId.class)
 public class VentaProducto {
 
     @Id
-    private Long idVenta;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Id
+    @ManyToOne
+    @JoinColumn(name = "venta_id", nullable = false)
+    @JsonIgnore
+    private Venta venta;
+
+    @Column(nullable = false)
     private Long idProducto;
 
     @Column(nullable = false)
